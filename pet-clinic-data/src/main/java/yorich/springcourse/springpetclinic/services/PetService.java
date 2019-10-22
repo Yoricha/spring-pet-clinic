80956a525bd07164bef9bf91506a2947e74a3bfa
Yoricha <grishaavramov@gmail.com>
2019-10-22 13:44:36 +0200

    rafactoring to common crud interfaces. Closes #11.

--- pet-clinic-data/src/main/java/yorich/springcourse/springpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/yorich/springcourse/springpetclinic/services/PetService.java
@@ -2,10 +2,5 @@ package yorich.springcourse.springpetclinic.services;
 
 import yorich.springcourse.springpetclinic.models.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById (Long id);
-    Pet save(Pet owner);
-    Set<Pet> findAll();
+public interface PetService extends  CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/yorich/springcourse/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/yorich/springcourse/springpetclinic/services/VetService.java
@@ -2,10 +2,5 @@ package yorich.springcourse.springpetclinic.services;
 
 import yorich.springcourse.springpetclinic.models.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById (Long id);
-    Vet save(Vet owner);
-    Set<Vet> findAll();
+public interface VetService  extends CrudService<Vet, Long> {
 }
