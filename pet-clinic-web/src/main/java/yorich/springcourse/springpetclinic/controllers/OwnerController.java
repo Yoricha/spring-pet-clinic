6648744980798559bfd1faee74ffde354e63c284
Yoricha <grishaavramov@gmail.com>
2019-10-23 10:43:33 +0200

    Owner index page and controller implemented. Closes #21.

---
+++ pet-clinic-web/src/main/java/yorich/springcourse/springpetclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package yorich.springcourse.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping({"/owners"})
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"","/","/index","/index.html"})
+    public String getAllOwners(){
+        return "owners/index";
+    }
+}
