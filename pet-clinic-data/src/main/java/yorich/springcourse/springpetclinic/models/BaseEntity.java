eb437e495773dbfd25a95c104e028c3fb79311ce
Yoricha <grishaavramov@gmail.com>
2019-10-23 10:22:27 +0200

    adding ID and GenerateValue annotations to BaseEntity

--- pet-clinic-data/src/main/java/yorich/springcourse/springpetclinic/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/yorich/springcourse/springpetclinic/models/BaseEntity.java
@@ -1,16 +1,15 @@
 package yorich.springcourse.springpetclinic.models;
 
-import java.io.Serializable;
+import lombok.Data;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+@Data
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
