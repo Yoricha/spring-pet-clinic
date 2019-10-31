489e71798f488e9b8d228a714a1e82af76048923
Yorich <grishaavramov@gmail.com>
2019-10-31 09:50:13 +0000

    implement PetType Service and Pet Service Impl

--- pet-clinic-data/src/main/java/yorich/springcourse/springpetclinic/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/yorich/springcourse/springpetclinic/models/BaseEntity.java
@@ -3,8 +3,16 @@ package yorich.springcourse.springpetclinic.models;
 import lombok.Data;
 
 import java.io.Serializable;
-@Data
+
 public class BaseEntity implements Serializable {
 
     private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
---
+++ pet-clinic-data/src/main/java/yorich/springcourse/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package yorich.springcourse.springpetclinic.services;
+
+import yorich.springcourse.springpetclinic.models.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
