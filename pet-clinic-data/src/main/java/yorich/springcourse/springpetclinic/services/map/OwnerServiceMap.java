c498d74713bd5c2849840d0b1a2b4416a382d316
Yorich <grishaavramov@gmail.com>
2019-10-31 12:42:48 +0000

    Adding details to ownsers. Assigning pets to owners and pet types to pet

--- pet-clinic-data/src/main/java/yorich/springcourse/springpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/yorich/springcourse/springpetclinic/services/map/OwnerServiceMap.java
@@ -2,12 +2,24 @@ package yorich.springcourse.springpetclinic.services.map;
 
 import org.springframework.stereotype.Service;
 import yorich.springcourse.springpetclinic.models.Owner;
+import yorich.springcourse.springpetclinic.models.Pet;
 import yorich.springcourse.springpetclinic.services.OwnerService;
+import yorich.springcourse.springpetclinic.services.PetService;
+import yorich.springcourse.springpetclinic.services.PetTypeService;
 
 import java.util.Set;
 
 @Service
-public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements OwnerService {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private PetService petService;
+    private PetTypeService petTypeService;
+
+    public OwnerServiceMap(PetService petService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.petTypeService = petTypeService;
+    }
+
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -25,7 +37,26 @@ public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements O
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+        if (object != null) {
+            if (object.getPets() != null) {
+                object.getPets().forEach(pet -> {
+                    if (pet.getPetType() != null) {
+                        if (pet.getPetType().getId() == null){
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    } else {
+                        throw new RuntimeException("Pet Type is Required");
+                    }
+                    if (pet.getId() == null){
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(object);
+        }else{
+            return null;
+        }
     }
 
     @Override
--- pet-clinic-web/src/main/java/yorich/springcourse/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/yorich/springcourse/springpetclinic/bootstrap/DataLoader.java
@@ -3,11 +3,14 @@ package yorich.springcourse.springpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import yorich.springcourse.springpetclinic.models.Owner;
+import yorich.springcourse.springpetclinic.models.Pet;
 import yorich.springcourse.springpetclinic.models.PetType;
 import yorich.springcourse.springpetclinic.models.Vet;
 import yorich.springcourse.springpetclinic.services.OwnerService;
 import yorich.springcourse.springpetclinic.services.PetTypeService;
 import yorich.springcourse.springpetclinic.services.VetService;
+
+import java.time.LocalDate;
 
 
 @Component
@@ -33,15 +36,34 @@ public class DataLoader implements CommandLineRunner {
         cat.setName("Pussy");
         PetType savedCat = petTypeService.save(cat);
 
-        Owner owner = new Owner();
-        owner.setFirstName("Grisha");
-        owner.setLastName("Avramov");
-        ownerService.save(owner);
 
-        Owner owner1 = new Owner();
-        owner1.setFirstName("Timea");
-        owner1.setLastName("Toth");
-        ownerService.save(owner1);
+        Pet grishaPet = new Pet();
+        grishaPet.setPetType(savedCat);
+        grishaPet.setBirthday(LocalDate.now());
+        grishaPet.setName("Yorich");
+
+        Pet timiPet = new Pet();
+        timiPet.setPetType(savedDog);
+        timiPet.setBirthday(LocalDate.of(2019,6,8));
+        timiPet.setName("Mirtil");
+
+        Owner grisha = new Owner();
+        grisha.setFirstName("Grisha");
+        grisha.setLastName("Avramov");
+        grisha.setAddress("ul. Orlovo gnezdo");
+        grisha.setCity("Ruse");
+        grisha.setTelephone("+359895363286");
+        grisha.getPets().add(grishaPet);
+        ownerService.save(grisha);
+
+        Owner timi = new Owner();
+        timi.setFirstName("Timea");
+        timi.setLastName("Toth");
+        timi.setAddress("Alacskai ut");
+        timi.setCity("Budapest 1188");
+        timi.setTelephone("+362024346124");
+        timi.getPets().add(timiPet);
+        ownerService.save(timi);
 
         Vet vet = new Vet();
         vet.setFirstName("Geza");
