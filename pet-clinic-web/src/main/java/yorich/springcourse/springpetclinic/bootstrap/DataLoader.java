962ca510236c1025534d476df614971445900cda
Yorich <grishaavramov@gmail.com>
2019-10-31 10:06:08 +0000

    adding pettype on start. Closes #33.

--- pet-clinic-web/src/main/java/yorich/springcourse/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/yorich/springcourse/springpetclinic/bootstrap/DataLoader.java
@@ -3,8 +3,10 @@ package yorich.springcourse.springpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import yorich.springcourse.springpetclinic.models.Owner;
+import yorich.springcourse.springpetclinic.models.PetType;
 import yorich.springcourse.springpetclinic.models.Vet;
 import yorich.springcourse.springpetclinic.services.OwnerService;
+import yorich.springcourse.springpetclinic.services.PetTypeService;
 import yorich.springcourse.springpetclinic.services.VetService;
 
 
@@ -13,14 +15,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Doge");
+        PetType savedDog = petTypeService.save(dog);
+        PetType cat = new PetType();
+        cat.setName("Pussy");
+        PetType savedCat = petTypeService.save(cat);
+
         Owner owner = new Owner();
         owner.setFirstName("Grisha");
         owner.setLastName("Avramov");
