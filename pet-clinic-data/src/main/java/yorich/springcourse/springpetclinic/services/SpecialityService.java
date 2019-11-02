4ba4235239d588ff6757c65d74f587f5c699d5d7
Yoricha <grishaavramov@gmail.com>
2019-11-02 11:21:08 +0100

    describe specialty impl. Closes #32.

---
+++ pet-clinic-data/src/main/java/yorich/springcourse/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package yorich.springcourse.springpetclinic.services;
+
+import yorich.springcourse.springpetclinic.models.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
