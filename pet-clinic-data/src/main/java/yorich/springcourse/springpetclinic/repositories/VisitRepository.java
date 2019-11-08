e0b3d97275468695d32efc724fd71fc8c6aebd59
Yoricha <grishaavramov@gmail.com>
2019-11-08 19:23:25 +0100

    A small mistake using VisitRepository as Type instead of Visit in CrudRepository interface

--- pet-clinic-data/src/main/java/yorich/springcourse/springpetclinic/repositories/VisitRepository.java
+++ pet-clinic-data/src/main/java/yorich/springcourse/springpetclinic/repositories/VisitRepository.java
@@ -1,6 +1,7 @@
 package yorich.springcourse.springpetclinic.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import yorich.springcourse.springpetclinic.models.Visit;
 
-public interface VisitRepository extends CrudRepository<VisitRepository, Long> {
+public interface VisitRepository extends CrudRepository<Visit, Long> {
 }
