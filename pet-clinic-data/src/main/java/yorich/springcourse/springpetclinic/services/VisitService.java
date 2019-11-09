d8419f3ce8c1c02aaec2758de979fa8075c18ae0
Yoricha <grishaavramov@gmail.com>
2019-11-09 12:42:58 +0100

    Implementing Map based Visit Service. Closes #47.

---
+++ pet-clinic-data/src/main/java/yorich/springcourse/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package yorich.springcourse.springpetclinic.services;
+
+import yorich.springcourse.springpetclinic.models.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
