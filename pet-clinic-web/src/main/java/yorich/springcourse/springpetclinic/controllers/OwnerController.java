9eee45fbb8c907c78726398671147b530b48e502
Yoricha <grishaavramov@gmail.com>
2020-01-25 12:15:44 +0100

    Adding onwer information

--- pet-clinic-web/src/main/java/yorich/springcourse/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/yorich/springcourse/springpetclinic/controllers/OwnerController.java
@@ -2,7 +2,10 @@ package yorich.springcourse.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import yorich.springcourse.springpetclinic.services.OwnerService;
 
 @RequestMapping({"/owners"})
@@ -15,15 +18,22 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"","/","/index","/index.html"})
-    public String getAllOwners(Model model){
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String getAllOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 
 
     @RequestMapping("/find")
-    public String findOwners(){
+    public String findOwners() {
         return "notimplemented";
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView showOwner(@PathVariable("id") Long id) {
+        ModelAndView modelAndView = new ModelAndView("owners/ownerDetails");
+        modelAndView.addObject(ownerService.findById(id));
+        return modelAndView;
     }
 }
